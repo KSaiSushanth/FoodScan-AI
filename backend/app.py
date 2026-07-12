1cb8e48e1dc5fcd7f03e4283a948bb20e84285fd
KSaiSushanth <sushanthsai626@gmail.com>
2026-07-12 18:50:23 +0530

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import io
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "allow_headers": "*", "methods": "*"}})
 
 # Load trained model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
